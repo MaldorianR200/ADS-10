04fcdc316f6e38351e7a8ab5473f5d98a08df40b
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-06-04 01:38:08 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,55 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+class Tree {
+private:
+	struct Node {
+		char c;
+		std::vector<Node*> vec;
+	};
+	Node* root;
+	std::vector<std::vector<char>>perms;
+
+
+
+public:
+		Tree(std::vector<char> new_vec) {
+		root = new Node;
+		root->c = ' ';
+		buildTree(root, new_vec);
+		std::vector<char> vector;
+		createPerms(root, vector);
+	}
+	std::vector <char> getPerm(const Tree& tree, int n);
+
+	void createPerms(Node* root, std::vector<char>vector) {
+		if (!root->vec.empty()) {
+			for (Node* next : root->vec) {
+				createPerms(next, vector);
+			}
+		}
+		else {
+			perms.push_back(vector);
+		}
+		if (!(root->c == ' ')) {
+			vector.push_back(root->c);
+		}
+
+	}
+
+	void buildTree(Node* root, std::vector<char>& vec) {
+
+		for (char c : vec) {
+			Node* temp = new Node;
+			temp->c = c;
+			root->vec.push_back(temp);
+			std::vector<char> remains = vec;
+			remains.erase(std::find(remains.begin(), remains.end(), c));
+			buildTree(temp, remains);
+		}
+	}
+	std::vector<std::vector<char>> getPerms()const {
+		return perms;
+	}
+};
 #endif  // INCLUDE_TREE_H_
